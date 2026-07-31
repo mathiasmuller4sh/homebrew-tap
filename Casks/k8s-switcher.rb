fbb46c2b7f46f207befad523ce2e04b6cb13d3d7
Mathias Muller <mathias.muller@4sh.fr>
2026-07-31 14:06:19 +0200

    Update k8s-switcher to v0.2.1

--- Casks/k8s-switcher.rb
+++ Casks/k8s-switcher.rb
@@ -1,8 +1,8 @@
 cask "k8s-switcher" do
-  version "0.1.21"
-  sha256 "e078cda620a8858667a50af4ed08498b8bf9c2695632219f1ea65dbf8307cae1"
+  version "0.2.1"
+  sha256 "a64c609586c10fe97d43fca8f58d5583f7b6025a7cf1351cf508ed7a1aa568bc"
 
-  url "https://github.com/mathiasmuller4sh/k8S-switcher/releases/download/v0.1.21/K8Switcher_0.1.21_aarch64.dmg"
+  url "https://github.com/mathiasmuller4sh/k8S-switcher/releases/download/v0.2.1/K8Switcher_0.2.1_aarch64.dmg"
   name "K8s Switcher"
   desc "Kubernetes cluster and namespace switcher"
   homepage "https://github.com/mathiasmuller4sh/k8S-switcher"
